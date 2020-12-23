c19ede13eed39665774ca9750412f70d08e1a458
ecastan960 <ecastan1@eafit.edu.co>
2020-12-22 22:18:53 -0500

    07|adding edge cases to main file

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
  */
 
 
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	FILE *fp;
 	stack_t *stack = NULL;
@@ -24,8 +24,18 @@ int main(int __attribute__((unused)) argc, char *argv[])
 
 	command = (char **)malloc(32 * sizeof(char *));
 
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 	delim = " ";
 	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		printf("Error: can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
 	while (fgets(line, 255, fp))
 	{
