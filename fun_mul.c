dfcbb2741ec724ca3eaf7ebcbe434974988087b2
jdanielue <jdurregoe@gmail.com>
2020-12-23 14:01:53 +0000

    new text description mul 1

--- fun_mul.c
+++ fun_mul.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * fun_mul - function that add the value 2 values on the front
+ * fun_mul - function that add the value 2 values on the front1
  * @stack: pointer  to the top
  * @line_c: Actual line of the file
  * Return: Always void.
