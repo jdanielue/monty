f0b2c151b2d45655bb0649d9ec55d8a2b2f4b438
jdanielue <jdurregoe@gmail.com>
2020-12-29 23:48:24 +0000

    edgecase v3

--- fun_swap.c
+++ fun_swap.c
@@ -9,16 +9,27 @@
 
 void fun_swap(stack_t **stack, unsigned int line_c)
 {
-	stack_t *temp1, *temp2;
+	stack_t *temp1, *temp2, *temp3;
+	int i;
+
+	temp3 = *stack;
 
 	if (*(stack) == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_c);
 		exit(EXIT_FAILURE);
 	}
+	for (i = 0; temp3 != NULL; i++)
+	{
+		temp3 = temp3->next;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_c);
+		exit(EXIT_FAILURE);
+	}
 
-	temp1 = malloc(sizeof(stack_t));
-	temp2 = malloc(sizeof(stack_t));
+
 	temp1 = *stack;
 	temp2 = (*stack)->next;
 	temp1->next = temp2->next;
